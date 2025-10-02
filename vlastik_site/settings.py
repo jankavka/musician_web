23ef4549b20a9f36a5845d217663cc0b5e41db6b
jankavka <jankavka@seznam.cz>
2025-10-02 16:58:45 +0200

    Some basic settings
    - Added bootstrap dep
    - Changed language of admin page
    - Added and registered classes Project and Event
    - Added first view with url setting.

--- vlastik_site/settings.py
+++ vlastik_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs'
 
 TIME_ZONE = 'Europe/Prague'
 
